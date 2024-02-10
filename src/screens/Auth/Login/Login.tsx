1aa0016898fd4bb4be5196e425db85ef222d6641
Emre Can Akyol <emrecanakyol@Emre-MacBook-Pro.local>
2024-02-10 17:52:10 +0300

    Login

--- src/screens/Auth/Login/Login.tsx
+++ src/screens/Auth/Login/Login.tsx
@@ -1,12 +1,212 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
+import {signInWithEmailAndPassword} from 'firebase/auth';
+import {auth} from '../../../../config/firebase';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {ToastError} from '../../../utils/ToastMessage';
 
-const Login = () => {
+export default function LoginScreen() {
+  const navigation: any = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    // Doğrulama işlemi
+    if (!email) {
+      setEmailError('E-posta adresi gerekli');
+      return;
+    }
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailError('Geçerli bir e-posta adresi girin');
+      return;
+    }
+    setEmailError('');
+    setLoading(true);
+
+    // Giriş işlemi
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err: any) {
+      console.log('Hata oluştu: ', err.message);
+      ToastError('Hata !', 'Giriş yapılamadı.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{flex: 1, backgroundColor: 'green'}}>
+      <SafeAreaView>
+        <View style={{flexDirection: 'row', justifyContent: 'flex-start'}}>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={{
+              backgroundColor: 'yellow',
+              padding: 10,
+              borderRadius: 20,
+              marginLeft: 10,
+            }}>
+            <Icon name="chevron-back-outline" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <Image
+            source={require('../../../assets/login.png')}
+            style={{width: 200, height: 200}}
+          />
+        </View>
+      </SafeAreaView>
+      <View
+        style={{
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          flex: 1,
+          backgroundColor: 'white',
+          paddingHorizontal: 20,
+          paddingTop: 20,
+        }}>
+        <ScrollView>
+          <View style={{marginBottom: 10, marginTop: 20}}>
+            <Text
+              style={{
+                color: '#555',
+                marginLeft: 12,
+                fontSize: 16,
+                fontWeight: '400',
+                marginBottom: 10,
+              }}>
+              E-Posta
+            </Text>
+            <TextInput
+              style={{
+                marginBottom: 20,
+                padding: 15,
+                backgroundColor: '#f0f0f0',
+                color: 'gray',
+                borderRadius: 20,
+                borderWidth: 1,
+                borderColor: emailError ? 'red' : '#f0f0f0',
+              }}
+              autoCapitalize="none"
+              value={email}
+              onChangeText={value => setEmail(value)}
+            />
+            {emailError ? (
+              <Text style={{color: 'red'}}>{emailError}</Text>
+            ) : null}
+            <Text
+              style={{
+                color: '#555',
+                marginLeft: 12,
+                fontSize: 16,
+                fontWeight: '400',
+                marginBottom: 10,
+              }}>
+              Şifre
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                backgroundColor: '#F3F4F6',
+                borderRadius: 20,
+                marginBottom: 10,
+              }}>
+              <TextInput
+                style={{
+                  flex: 1,
+                  padding: 15,
+                  color: 'gray',
+                  borderRadius: 20,
+                  marginRight: 10,
+                }}
+                autoCapitalize="none"
+                secureTextEntry={!showPassword}
+                placeholder="password"
+                value={password}
+                onChangeText={value => setPassword(value)}
+              />
+              <TouchableOpacity
+                onPress={() => setShowPassword(!showPassword)}
+                style={{padding: 10}}>
+                <Icon
+                  name={showPassword ? 'eye-off-outline' : 'eye-outline'}
+                  size={24}
+                  color="gray"
+                />
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity>
+              <Text
+                style={{color: 'gray', textAlign: 'right', marginBottom: 50}}>
+                Şifrenizi mi unuttunuz?
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handleSubmit}
+              style={{
+                backgroundColor: '#FFD700',
+                paddingVertical: 15,
+                borderRadius: 20,
+                opacity: loading ? 0.5 : 1,
+              }}
+              disabled={loading}>
+              {loading ? (
+                <ActivityIndicator color="gray" />
+              ) : (
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: 'gray',
+                    textAlign: 'center',
+                  }}>
+                  Giriş Yap
+                </Text>
+              )}
+            </TouchableOpacity>
+          </View>
+          <Text
+            style={{
+              fontSize: 20,
+              color: 'gray',
+              fontWeight: 'bold',
+              textAlign: 'center',
+              marginVertical: 20,
+            }}>
+            veya
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              marginBottom: 300,
+            }}>
+            <Text style={{color: 'gray', fontWeight: 'bold'}}>
+              Hesabınız yok mu?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+              <Text
+                style={{fontWeight: '800', color: '#FFD700', marginLeft: 10}}>
+                Kayıt Ol
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </View>
     </View>
-  )
+  );
 }
-
-export default Login
--- src/screens/Auth/Register/Register.tsx
+++ src/screens/Auth/Register/Register.tsx
@@ -1,10 +1,19 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, Image, TextInput, ScrollView, ActivityIndicator} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useNavigation} from '@react-navigation/native';
 import {createUserWithEmailAndPassword} from 'firebase/auth';
 import {auth} from '../../../../config/firebase';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { ToastError } from '../../../utils/ToastMessage';
 
 type SignUpScreenProps = {};
 
@@ -44,12 +53,19 @@ export default function SignUpScreen(props: SignUpScreenProps) {
   };
 
   const handleSubmit = async () => {
-    if (email && password && confirmPassword && validateEmail(email) && validatePassword(password, confirmPassword)) {
+    if (
+      email &&
+      password &&
+      confirmPassword &&
+      validateEmail(email) &&
+      validatePassword(password, confirmPassword)
+    ) {
       setLoading(true);
       try {
         await createUserWithEmailAndPassword(auth, email, password);
       } catch (err: any) {
         console.log('got error: ', err.message);
+        ToastError("Hata !", err.message);
       } finally {
         setLoading(false);
       }
@@ -87,141 +103,174 @@ export default function SignUpScreen(props: SignUpScreenProps) {
           borderTopLeftRadius: 50,
           borderTopRightRadius: 50,
         }}>
-        <ScrollView >
-
-        <View style={{marginBottom: 7, gap: 10, marginTop: 20}}>
-          <Text
-            style={{
-              color: '#555',
-              marginLeft: 12,
-              fontSize: 16,
-              fontWeight: '400',
-            }}>
-            E-Posta
-          </Text>
-          <TextInput
-            style={{
-              padding: 16,
-              backgroundColor: '#F3F4F6',
-              color: '#555555',
-              borderRadius: 20,
-              marginBottom: 3,
-              borderColor: emailError ? 'red' : '#F3F4F6',
-              borderWidth: emailError ? 1 : 0,
-            }}
-            value={email}
-            onChangeText={value => setEmail(value)}
-          />
-          {emailError ? (
-            <Text style={{color: 'red', marginLeft: 12}}>{emailError}</Text>
-          ) : null}
-          <Text
-            style={{
-              color: '#555',
-              marginLeft: 12,
-              fontSize: 16,
-              fontWeight: '400',
-            }}>
-            Şifre
-          </Text>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <TextInput
+        <ScrollView>
+          <View style={{marginBottom: 7,  marginTop: 20}}>
+            <Text
               style={{
-                flex: 1,
-                padding: 16,
-                backgroundColor: '#F3F4F6',
-                color: '#555555',
-                borderRadius: 20,
-                marginBottom: 3,
-                borderColor: passwordError ? 'red' : '#F3F4F6',
-                borderWidth: passwordError ? 1 : 0,
-              }}
-              secureTextEntry={!showPassword}
-              value={password}
-              onChangeText={value => setPassword(value)}
-            />
-            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-              <Icon name={showPassword ? "eye-off-outline" : "eye-outline"} size={20} color="#555555" style={{marginRight: 16}} />
-            </TouchableOpacity>
-          </View>
-          <Text
-            style={{
-              color: '#555',
-              marginLeft: 12,
-              fontSize: 16,
-              fontWeight: '400',
-            }}>
-            Şifre Doğrulama
-          </Text>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <TextInput
-              style={{
-                flex: 1,
-                padding: 16,
-                backgroundColor: '#F3F4F6',
-                color: '#555555',
-                borderRadius: 20,
-                marginBottom: 7,
-                borderColor: passwordError ? 'red' : '#F3F4F6',
-                borderWidth: passwordError ? 1 : 0,
-              }}
-              secureTextEntry={!showConfirmPassword}
-              value={confirmPassword}
-              onChangeText={value => setConfirmPassword(value)}
-            />
-            <TouchableOpacity onPress={() => setShowConfirmPassword(!showConfirmPassword)}>
-              <Icon name={showConfirmPassword ? "eye-off-outline" : "eye-outline"} size={20} color="#555555" style={{marginRight: 16}} />
-            </TouchableOpacity>
-          </View>
-          {passwordError ? (
-            <Text style={{color: 'red', marginLeft: 12}}>{passwordError}</Text>
-          ) : null}
-          <TouchableOpacity
-            style={{padding: 12, backgroundColor: '#FFD700', borderRadius: 20}}
-            onPress={handleSubmit}
-            disabled={loading}
-          >
-            {loading ? (
-              <ActivityIndicator color="#555555" />
-            ) : (
-              <Text
-                style={{
-                  fontSize: 20,
-                  fontWeight: 'bold',
-                  textAlign: 'center',
-                  color: '#555555',
-                }}>
-                Kayıt Ol
-              </Text>
-            )}
-          </TouchableOpacity>
-        </View>
-        <Text
-          style={{
-            fontSize: 20,
-            color: '#555555',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            paddingBottom: 5,
-          }}>
-          veya
-        </Text>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            marginTop: 7,
-            marginBottom: 310,
-          }}>
-          <Text style={{color: '#888888', fontWeight: '600'}}>
-            Zaten hesabın var mı?
-          </Text>
-          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-            <Text style={{fontWeight: '800', color: '#FFD700', marginLeft: 10}}>
-              Giriş Yap
+                color: '#555',
+                marginLeft: 12,
+                fontSize: 16,
+                fontWeight: '400',
+                marginBottom: 10,
+              }}>
+              E-Posta
             </Text>
-          </TouchableOpacity>
-        </View>
+            <TextInput
+              style={{
+                padding: 16,
+                backgroundColor: '#F3F4F6',
+                color: '#555555',
+                borderRadius: 20,
+                marginBottom: 20,
+                borderColor: emailError ? 'red' : '#F3F4F6',
+                borderWidth: emailError ? 1 : 0,
+              }}
+              autoCapitalize="none"
+              value={email}
+              onChangeText={value => setEmail(value)}
+            />
+            {emailError ? (
+              <Text style={{color: 'red', marginLeft: 12}}>{emailError}</Text>
+            ) : null}
+            <Text
+              style={{
+                color: '#555',
+                marginLeft: 12,
+                fontSize: 16,
+                fontWeight: '400',
+                marginBottom: 10,
+              }}>
+              Şifre
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                backgroundColor: '#F3F4F6',
+                borderRadius: 20,
+                marginBottom: 20,
+              }}>
+              <TextInput
+                style={{
+                  flex: 1,
+                  padding: 16,
+                  color: '#555555',
+                  borderRadius: 20,
+                  borderColor: passwordError ? 'red' : '#F3F4F6',
+                  borderWidth: passwordError ? 1 : 0,
+                }}
+                autoCapitalize="none"
+                secureTextEntry={!showPassword}
+                value={password}
+                onChangeText={value => setPassword(value)}
+              />
+              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <Icon
+                  name={showPassword ? 'eye-off-outline' : 'eye-outline'}
+                  size={20}
+                  color="#555555"
+                  style={{marginRight: 16}}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text
+              style={{
+                color: '#555',
+                marginLeft: 12,
+                fontSize: 16,
+                fontWeight: '400',
+                marginBottom: 10,
+              }}>
+              Şifre Doğrulama
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                backgroundColor: '#F3F4F6',
+                borderRadius: 20,
+                marginBottom: 50,
+              }}>
+              <TextInput
+                style={{
+                  flex: 1,
+                  padding: 16,
+                  color: '#555555',
+                  borderRadius: 20,
+                  borderColor: passwordError ? 'red' : '#F3F4F6',
+                  borderWidth: passwordError ? 1 : 0,
+                }}
+                autoCapitalize="none"
+                secureTextEntry={!showConfirmPassword}
+                value={confirmPassword}
+                onChangeText={value => setConfirmPassword(value)}
+              />
+              <TouchableOpacity
+                onPress={() => setShowConfirmPassword(!showConfirmPassword)}>
+                <Icon
+                  name={showConfirmPassword ? 'eye-off-outline' : 'eye-outline'}
+                  size={20}
+                  color="#555555"
+                  style={{marginRight: 16}}
+                />
+              </TouchableOpacity>
+            </View>
+            {passwordError ? (
+              <Text style={{color: 'red', marginLeft: 12}}>
+                {passwordError}
+              </Text>
+            ) : null}
+            <TouchableOpacity
+              style={{
+                padding: 12,
+                backgroundColor: '#FFD700',
+                borderRadius: 20,
+              }}
+              onPress={handleSubmit}
+              disabled={loading}>
+              {loading ? (
+                <ActivityIndicator color="#555555" />
+              ) : (
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    color: '#555555',
+                  }}>
+                  Kayıt Ol
+                </Text>
+              )}
+            </TouchableOpacity>
+          </View>
+          <Text
+            style={{
+              fontSize: 20,
+              color: '#555555',
+              fontWeight: 'bold',
+              textAlign: 'center',
+              marginTop: 16,
+              marginBottom: 10,
+            }}>
+            veya
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              marginTop: 7,
+              marginBottom: 310,
+            }}>
+            <Text style={{color: '#888888', fontWeight: '600'}}>
+              Zaten hesabın var mı?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+              <Text
+                style={{fontWeight: '800', color: '#FFD700', marginLeft: 10}}>
+                Giriş Yap
+              </Text>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
       </View>
     </View>
